bd4e85a59c81ce4852f933e69f98b72e7cf3793b
mazidhussain <mazidhussain71@gmail.com>
2023-03-21 23:17:04 +0530

    airbnb backend setup

---
+++ index.js
@@ -0,0 +1,206 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+const bycrypt = require('bcryptjs');
+const User = require('./models/User.js');
+const Place = require('./models/Place.js'); 
+const Booking = require('./models/Booking.js');
+const jwt = require('jsonwebtoken');
+const imageDownloader = require('image-downloader');
+const fs = require('fs');
+const multer = require('multer');
+mongoose.set("strictQuery", false);
+app.use(express.json());
+const cookieParser = require('cookie-parser');
+const { rejects } = require('assert');
+require('dotenv').config();
+
+app.use(cookieParser());
+app.use('/uploads',express.static(__dirname+'/uploads'));
+const bycryptSalt = bycrypt.genSaltSync(10);
+const jwtSecret = 'MazidHussainAnsari';
+
+app.use(cors({
+    credentials:true,
+    origin:'https://6412a6eae57bc8321c7afdab--euphonious-marzipan-5dc245.netlify.app',
+}));
+mongoose.connect(process.env.MONGO_URL);
+
+function getUserDataFromReq(req){
+    return new Promise((resolve,reject)=>{
+        jwt.verify(req.cookies.token,jwtSecret,{},async (err,userData)=>{
+            if(err) throw err;
+            resolve(userData);
+        });
+    });
+}
+
+app.get('/test',(req,res)=>{
+    res.json('test ok');
+});
+
+// for registration
+app.post('/register',async (req,res)=>{
+    const {name,email,password} = req.body;
+    try{
+        const userDoc = await User.create({
+            name,
+            email,
+            password:bycrypt.hashSync(password,bycryptSalt),
+        });
+        res.json(userDoc);
+    }catch(e){
+         res.status(422).json(e);
+    }
+});
+
+// for login
+app.post('/login',async (req,res)=>{
+    const {email,password} = req.body;
+    const userDoc = await User.findOne({email});
+    if(userDoc){
+        const passOk = bycrypt.compareSync(password,userDoc.password);
+        if(passOk){
+            jwt.sign({email:userDoc.email,id:userDoc._id},jwtSecret,{},(err,token)=>{
+                if(err){
+                    throw err;
+                }
+                res.cookie('token',token).json(userDoc);
+            });
+        }else{
+            res.status(422).json('pass not ok');
+        }
+    }else{
+        res.json('not found');
+    }
+});
+
+// for token fetch
+app.get('/profile',(req,res)=>{
+    const {token} = req.cookies;
+    if(token){
+        jwt.verify(token,jwtSecret,{},async (err,userData)=>{
+            if(err) throw err;
+            const {name,email,_id} = await User.findById(userData.id);
+            res.json({name,email,_id});
+        });
+    }else{
+        res.json(null);
+    }
+});
+
+// for logout
+app.post('/logout',(req,res)=>{
+    res.cookie('token','').json(true);
+});
+
+// for upload by link
+app.post('/upload-by-link',async (req,res)=>{
+    const {link} = req.body;
+    const newName = 'photo'+Date.now()+'.jpg';
+    await imageDownloader.image({
+        url:link,
+        dest: __dirname + '/uploads/'+newName
+    });
+    res.json(newName);
+}); 
+
+// for upload from system
+const photoMiddleware = multer({dest:'uploads'});
+app.post('/upload',photoMiddleware.array('photos',100),(req,res)=>{
+    const uploadedFiles = [];
+    for(let i=0;i<req.files.length ;i++){
+        const {path,originalname} = req.files[i];
+        const parts = originalname.split('.');
+        const newPath = path + '.'+parts[parts.length-1];
+        fs.renameSync(path,newPath);
+        uploadedFiles.push(newPath.replace('uploads\\',''));
+    }
+    res.json(uploadedFiles);
+});
+
+// for new place add form data
+app.post('/places',(req,res)=>{
+    const {token} = req.cookies;
+    jwt.verify(token,jwtSecret,{},async (err,userData)=>{
+        const {title,address,addedPhotos,
+            description,perks,extraInfo,
+            checkIn,checkOut,maxGuests,price} = req.body;
+        if(err) throw err;
+        const placeDoc = await Place.create({
+            owner:userData.id,
+            title,address,photos:addedPhotos,
+            description,perks,extraInfo,
+            checkIn,checkOut,maxGuests,price
+        });
+        res.json(placeDoc);
+    });
+});
+
+// for fectch data
+app.get('/user-places',(req,res)=>{
+    const {token} = req.cookies;
+    jwt.verify(token,jwtSecret,{},async (err,userData)=>{
+        if(err) throw err;
+        const {id} = userData;
+        res.json(await Place.find({owner:id}));
+    });
+
+});
+
+app.get('/places/:id',async (req,res)=>{
+    const {id} = req.params;
+    res.json(await Place.findById(id));
+});
+
+app.put('/places',async(req,res)=>{
+    const {token} = req.cookies;
+    const {
+        id,title,address,addedPhotos,
+        description,perks,extraInfo,
+        checkIn,checkOut,maxGuests,price
+    } = req.body;
+    jwt.verify(token,jwtSecret,{},async (err,userData)=>{
+        const placeDoc = await Place.findById(id);
+        if(err) throw err;
+        if(userData.id === placeDoc.owner.toString()){
+            placeDoc.set({title,address,photos:addedPhotos,
+                description,perks,extraInfo,
+                checkIn,checkOut,maxGuests,price});
+            await placeDoc.save();
+            res.json('updated successfully');
+        }
+    });
+});
+
+
+// for fetch data  in home page
+app.get('/places',async (req,res)=>{
+    res.json(await Place.find());
+}); 
+
+//for booking submit
+app.post('/booking',async (req,res)=>{
+    const userData = await getUserDataFromReq(req);
+    const {place,checkIn,checkOut,numberOfGuests,name,phone,price} = req.body;
+    Booking.create({
+        place,checkIn,checkOut,numberOfGuests,name,phone,price,user:userData.id
+    }).then((doc)=>{
+        res.json(doc);
+    }).catch((err)=>{
+        throw err;
+    });
+
+});
+
+const PORT = process.env.PORT || 4000;
+// for fetch bookings all
+app.get('/bookings',async(req,res)=>{
+    const userData = await getUserDataFromReq(req);
+    res.json(await Booking.find({user:userData.id }).populate('place'));
+});
+app.listen(PORT, function(err){
+    if (err) console.log("Error in server setup")
+    console.log("Server listening on Port", PORT);
+})
